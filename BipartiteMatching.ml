b692058c0d1d90fd07f652619c25853337427d2c
unknown <laurabouzereau@hotmail.fr>
2019-01-08 15:05:29 +0100

    yolo

--- BipartiteMatching.ml
+++ BipartiteMatching.ml
@@ -2,14 +2,16 @@ open Graph
 open FF
 open Printf
 type path = string
+let shift label=string_of_int((int_of_string label)+2)
+let unshift label=string_of_int((int_of_string label)-2)
 let read_x graph line=
-  try Scanf.sscanf line "x %s" (fun id-> add_arc (add_node graph (string_of_int ((int_of_string id)+2))) "0" (string_of_int((int_of_string id) + 2)) "1")
+  try Scanf.sscanf line "x %s" (fun id-> add_arc (add_node graph (shift id)) "0" (shift id) "1")
   with e -> failwith "convert"
 let read_y graph line=
-  try Scanf.sscanf line "y %s" (fun id-> add_arc (add_node graph (string_of_int ((int_of_string id)+2))) (string_of_int ((int_of_string id)+2)) "1" "1")
+  try Scanf.sscanf line "y %s" (fun id-> add_arc (add_node graph (shift id)) (shift id) "1" "1")
   with e -> failwith "convert"
 let read_e graph line=
-  try Scanf.sscanf line "e %s %s" (fun id1 id2 -> add_arc graph id1 id2 "1")
+  try Scanf.sscanf line "e %s %s" (fun id1 id2 -> add_arc graph (shift id1) (shift id2) "1")
   with e -> failwith "convert"
 (* convert : path -> string graph*)
 let convert path=
@@ -32,7 +34,28 @@ let convert path=
   let final_graph = loop (add_node (add_node empty_graph "0") "1") in
     close_in infile;
     final_graph
-(*
+
 (* filter : int graph -> string graph*)
 let filter graph=
-*)
+  let rec ajoutnode nodes gr_acu_nodes=
+    match nodes with
+      | "0" :: rest -> ajoutnode rest gr_acu_nodes
+      | "1" :: rest -> ajoutnode rest gr_acu_nodes
+      | id :: rest -> ajoutnode rest (add_node gr_acu_nodes (unshift id))
+      | _ -> gr_acu_nodes
+  in
+  let ajout_arc acu idf=
+    try
+      let (id,flot)=List.find (fun (id, flot) -> (not(id="1") && flot==1)) (out_arcs graph idf) 
+      in add_arc acu (unshift idf) (unshift id) "1"
+    with Not_found -> acu
+  in
+  let rec loopnode nodes gr_acu=
+    match nodes with
+      | "0" :: rest -> loopnode rest gr_acu
+      | "1" :: rest -> loopnode rest gr_acu
+      | id :: rest -> loopnode rest (ajout_arc gr_acu id)
+      | _ -> gr_acu
+  in
+    loopnode (out_nodes graph) (ajoutnode (out_nodes graph) empty_graph)
+
